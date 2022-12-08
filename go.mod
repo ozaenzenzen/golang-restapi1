5877969d2422fc9ac812d10e176158aa63674854
Fauzan Akmal Mahdi <fauzan@loyalto.id>
2022-12-08 14:15:29 +0700

    restapi golang

---
+++ go.mod
@@ -0,0 +1,5 @@
+module golang-api2
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Article struct {
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"Desc"`
+	Content string `json:"Content"`
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	// fmt.Fprintf(w, "key: "+key)
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	// fmt.Fprintf(w, "%+v", string(reqBody))
+	var article Article
+	json.Unmarshal(reqBody, &article)
+	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	// once again, we will need to parse the path parameters
+	vars := mux.Vars(r)
+	// we will need to extract the `id` of the article we
+	// wish to delete
+	id := vars["id"]
+
+	// we then need to loop through all our articles
+	for index, article := range Articles {
+		// if our id path parameter matches one of our
+		// articles
+		if article.Id == id {
+			// updates our Articles array to remove the
+			// article
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+	fmt.Println("Golang serve at localhost:10000")
+}
+
+func handleRequests2() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func closeAct() {
+	fmt.Println("TEST AJAAAA")
+}
+
+var Articles []Article
+
+func handleArticle() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	Articles = []Article{
+		Article{Id: "1", Title: "Hello", Desc: "Article Description 1", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description 2", Content: "Article Content"},
+		Article{Id: "3", Title: "Hello 3", Desc: "Article Description 3", Content: "Article Content"},
+	}
+	handleRequests2()
+}
+
+func main() {
+	handleArticle()
+}
